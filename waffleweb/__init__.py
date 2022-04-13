34be240591a78b9807136a79262e04010d7a8d71
Berserkware <berserkware@gmail.com>
2022-04-13 07:52:31 +0000

    Added registar for views

---
+++ waffleweb/__init__.py
@@ -0,0 +1,3 @@
+from .app import waffleApp
+
+__version__ = "0.1.0"
--- waffleweb/app.py
+++ waffleweb/app.py
@@ -1,11 +1,26 @@
 import re
 
 class waffleApp():
-    def __init__(self, appName):
+    '''
+    The waffleApp() class is the centre of all the apps for your project.
+    It only takes one argument: name, which is the name of your
+    application. It is automatically defined when you create an app as so:
+    app = waffleApp('yourAppName')
+    '''
+
+    def __init__(self, appName: str):
         self.appName = appName
         self.views = []
 
-    def route(self, path, name):
+    @property
+    def views(self):
+        return self._views
+
+    @views.setter
+    def views(self, value):
+        self._views = value
+
+    def route(self, path: str, name: str):
         '''
         This is the decorator you put on all your views it gives your view a URL and a name.
         It takes two arguments path and name. The path argument is the reletive URL to your 
@@ -24,19 +39,13 @@ class waffleApp():
         '''
         
         def decorator(view):
-            print('dec')
-            def wrapper(*args, **kwargs):
-                print('wrap')
-                #regex from https://stackoverflow.com/questions/31430167/regex-check-if-given-string-is-relative-url
-                if re.compile(r'^(?!www\.|(?:http|ftp)s?://|[A-Za-z]:\\|//).*').search(path):
+            #regex from https://stackoverflow.com/questions/31430167/regex-check-if-given-string-is-relative-url
+            if re.compile(r'^(?!www\.|(?:http|ftp)s?://|[A-Za-z]:\\|//).*').search(path):
+                self.views.append({'path': path, 'name': name})
+
+                def wrapper(*args, **kwargs):
                     view(*args, **kwargs)
-                else:
-                    raise ValueError('Your path has to be a valid reletive URL pattern.')
-            return wrapper
-        return decorator
-
-app = waffleApp('mainPage')
-
-@app.route('index/', 'index')
-def index(request=None):
-    pass
+                return wrapper
+            else:
+                raise ValueError('Your path has to be a valid relative URL pattern.')
+        return decorator 
